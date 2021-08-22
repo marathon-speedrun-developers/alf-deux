2c6eac9d40649e5948eb2f52f3f97458f174a22b
Chris Staples <stapleschris0@gmail.com>
2021-08-21 23:10:53 -0600

    Added following backport from A1 - 606411dec845266871c339ba2705014d67855b2e : Fix memory leak , from upstream
    Aug 08, 2021 (Obsidian)
      Added following backport from A1 - 606411dec845266871c339ba2705014d67855b2e : Fix memory leak

--- Source_Files/RenderMain/shape_definitions.h
+++ Source_Files/RenderMain/shape_definitions.h
@@ -26,6 +26,9 @@ Aug 14, 2000 (Loren Petrich):
 	Turned collection and shading-table handles into pointers,
 	because handles are needlessly MacOS-specific,
 	and because these are variable-format objects.
+
+Aug 08, 2021 (Obsidian)
+  Added following backport from A1 - 606411dec845266871c339ba2705014d67855b2e : Fix memory leak
 */
 
 #include "shape_descriptors.h"
@@ -43,8 +46,8 @@ struct collection_header /* 32 bytes on disk */
 	int32 offset16, length16;
 
 	// LP: handles to pointers
-	collection_definition *collection;
-	byte *shading_tables;
+	collection_definition* collection;
+	std::vector<byte> shading_tables;
 };
 const int SIZEOF_collection_header = 32;
 
--- Source_Files/RenderMain/shapes.cpp
+++ Source_Files/RenderMain/shapes.cpp
@@ -73,6 +73,8 @@ Sept 2, 2000 (Loren Petrich):
 
 Jan 17, 2001 (Loren Petrich):
 	Added support for offsets for OpenGL-rendered substitute textures
+Aug 08, 2021 (Obsidian)
+  Added following backport from A1 - 606411dec845266871c339ba2705014d67855b2e : Fix memory leak
 */
 
 /*
@@ -736,10 +738,10 @@ static void allocate_shading_tables(short collection_index, bool strip)
 	collection_header *header = get_collection_header(collection_index);
 	// Allocate enough space for this collection's shading tables
 	if (strip)
-		header->shading_tables = NULL;
+		header->shading_tables.clear();
 	else {
 		collection_definition *definition = get_collection_definition(collection_index);
-		header->shading_tables = (byte *)malloc(get_shading_table_size(collection_index) * definition->clut_count + shading_table_size * NUMBER_OF_TINT_TABLES);
+		header->shading_tables.resize(get_shading_table_size(collection_index) * definition->clut_count + shading_table_size * NUMBER_OF_TINT_TABLES);
 	}
 }
 
@@ -848,7 +850,7 @@ static bool load_collection(short collection_index, bool strip)
 
 	allocate_shading_tables(collection_index, strip);
 	
-	if (header->shading_tables == NULL) {
+	if (header->shading_tables.empty()) {
 		delete header->collection;
 		header->collection = NULL;
 		return false;
@@ -867,9 +869,9 @@ static void unload_collection(struct collection_header *header)
 {
 	assert(header->collection);
 	delete header->collection;
-	free(header->shading_tables);
+	header->shading_tables.clear();
 	header->collection = NULL;
-	header->shading_tables = NULL;
+	//header->shading_tables = NULL;
 }
 
 #define ENDC_TAG FOUR_CHARS_TO_INT('e', 'n', 'd', 'c')
@@ -1084,7 +1086,7 @@ void open_shapes_file(FileSpecifier& File)
 			S += 6*2;
 			
 			ObjPtr->collection = NULL;	// so unloading can work properly
-			ObjPtr->shading_tables = NULL;	// so unloading can work properly
+			ObjPtr->shading_tables.clear();	// so unloading can work properly
 		}
 		
 		assert((S - CollHdrStream) == Count*SIZEOF_collection_header);
@@ -2363,8 +2365,7 @@ static void *get_collection_shading_tables(
 	short collection_index,
 	short clut_index)
 {
-	void *shading_tables= get_collection_header(collection_index)->shading_tables;
-
+	void* shading_tables = get_collection_header(collection_index)->shading_tables.data();
 	shading_tables = (uint8 *)shading_tables + clut_index*get_shading_table_size(collection_index);
 	
 	return shading_tables;
@@ -2377,7 +2378,7 @@ static void *get_collection_tint_tables(
 	struct collection_definition *definition= get_collection_definition(collection_index);
 	if (!definition) return NULL;
 	
-	void *tint_table= get_collection_header(collection_index)->shading_tables;
+	void* tint_table = get_collection_header(collection_index)->shading_tables.data();
 
 	tint_table = (uint8 *)tint_table + get_shading_table_size(collection_index)*definition->clut_count + shading_table_size*tint_index;
 	
